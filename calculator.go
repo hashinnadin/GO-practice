b45ab60c0cc417f39b799f6473d3466b0b883dd6
hashinnadin <hashinnadin@gmail.com>
2026-02-09 17:32:52 +0530

    update

--- calculator.go
+++ calculator.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func aain() {
 	var a, b int
 	var op string
 
--- main.go
+++ main.go
@@ -10,24 +10,45 @@
 // 	fmt.Println(slice)
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	students := make(map[int]string)
+
+// 	students[1] = "HAshin"
+// 	students[2] = "shaju"
+// 	students[3] = "anu"
+// 	students[4] = "mnu"
+// 	students[5] = "snu"
+// 	students[6] = "lnu"
+// 	students[7] = "Hanan"
+
+// 	delete(students, 2)
+
+// 	for id, name := range students {
+// 		fmt.Println(id, name)
+// 	}
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	students := make(map[int]string)
+	nums := []int{10, 20, 30, 40, 6, 5}
+	max := nums[0]
+	min := nums[0]
 
-	students[1] = "HAshin"
-	students[2] = "shaju"
-	students[3] = "anu"
-	students[4] = "mnu"
-	students[5] = "snu"
-	students[6] = "lnu"
-	students[7] = "Hanan"
-
-	delete(students, 2)
-
-	for id, name := range students {
-		fmt.Println(id, name)
+	for _, v := range nums {
+		if v > max {
+			max = v
+		}
+		if v < min {
+			min = v
+		}
 	}
+	fmt.Println("MAx :", max)
+	fmt.Println("Min :", min)
 }
